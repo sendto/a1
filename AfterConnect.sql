d16f62d942a7612df2d5fc613491d40c1cdf4ffd
sendto <sendtoa01@gmail.com>
2017-11-22 12:58:33 +0300

    Create AfterConnect.sql

---
+++ AfterConnect.sql
@@ -0,0 +1,5 @@
+alter session set nls_date_format = 'dd.mm.yyyy';
+alter session set nls_time_format = 'hh24:mi:ssxff';
+alter session set nls_timestamp_format = 'dd.mm.yyyy hh24:mi:ssxff';
+alter session set nls_time_tz_format = 'hh24:mi:ssxff tzr';
+alter session set nls_timestamp_tz_format = 'dd.mm.yyyy hh24:mi:ssxff tzr';
